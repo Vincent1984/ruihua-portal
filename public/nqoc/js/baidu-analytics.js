7d7803a880449044e55822250ffa00fbcd13a3a8
niczhangjian01-oss <niczhangjian01@gmail.com>
2026-09-10 13:50:16 +0800

    更新
    新质组织统计代码

---
+++ public/nqoc/js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fa4ab6a16c12c6034f60f5c597b5b318";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
